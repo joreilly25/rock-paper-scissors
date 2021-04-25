ce39d911c7e4cf53ccf290fbc167396d9fa44cab
Justin O'Reilly <joreilly25@gmail.com>
2021-04-25 16:11:04 -0400

    added playRound() function

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -5,11 +5,11 @@
 // create a function to take these values and compare them to delcare a winner | this will be each round of the game
 // create a function to wrap all of these functions and loop through muiltiple games
 // the game function should keep score (5 games) and delcare a winner at the end
+// Rock beats Scissors, Scissors beats Paper, Paper beats Rock
 
 function computerPlay() {
     let randNum = Math.floor(Math.random() * 3);
     let compSelection;
-    
     if (randNum < 1) {
         compSelection = "rock";
     } else if (randNum == 1) {
@@ -19,4 +19,30 @@ function computerPlay() {
     }
     return compSelection;
 }
-console.log(computerPlay());
+const compSelection = computerPlay();
+console.log("computer: " + compSelection);
+
+const playerSelection = prompt("Please enter \"Rock, Paper, or Scissors\"").toLowerCase();
+
+console.log("player: " + playerSelection);
+
+function playRound(playerSelection, compSelection) {
+    if (playerSelection == "rock" && compSelection == "scissors") {
+        return console.log("You Win! Rock beats Scissors");
+    }
+    else if (playerSelection == "scissors" && compSelection == "rock") {
+        return console.log("You Lose! Rock beats Scissors");
+    } else if (playerSelection == "rock" && compSelection == "paper") {
+        return console.log("You Lose! Paper beats Rock!");
+    } else if (playerSelection == "paper" && compSelection == "rock") {
+        return console.log("You Win! Paper beats Rock!");
+    } else if (playerSelection == "paper" && compSelection == "scissors") {
+        return console.log("You Lose! Scissors beats paper!");
+    } else if (playerSelection == "scissors" && compSelection == "paper") {
+        return console.log("You Win! Scissors beats paper!");
+    } else {
+        return console.log("Draw! " + playerSelection + " vs " + compSelection + "!")
+    }
+}
+
+playRound(playerSelection, compSelection);
