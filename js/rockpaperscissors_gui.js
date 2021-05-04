06699877fb47c6e2ff1b6ef5b4b36e1eca3c87a0
Justin O'Reilly <joreilly25@gmail.com>
2021-05-04 17:24:28 -0400

    updated with GUI still WIP

---
+++ js/rockpaperscissors_gui.js
@@ -0,0 +1,84 @@
+// function will need to generate a random number out of 3 total options - done
+// depending on that number it will assign either rock, paper or scissors and return that value - done
+// function will need to take user input of either rock, paper, or scissors anything else should result in a try again
+// function will return the user's inputed value - needed?
+// create a function to take these values and compare them to delcare a winner | this will be each round of the game - done
+// create a function to wrap all of these functions and loop through muiltiple games - done
+// the game function should keep score (5 games) and delcare a winner at the end - done
+// Rock beats Scissors, Scissors beats Paper, Paper beats Rock
+
+let playerScore = 0;
+let compScore = 0;
+const selections = document.querySelectorAll("button");
+
+selections.forEach((button) => {
+  button.addEventListener("click", () => {
+    let playerSelection = button.id;
+    console.log(`You selected: ${playerSelection}`);
+    computerPlay();
+    playRound(playerSelection, randCompSelector);
+  });
+});
+
+function computerPlay() {
+  let compSelector = ["rock", "paper", "scissors"];
+  randCompSelector = compSelector[Math.floor(Math.random() * compSelector.length)];
+  console.log(`Computer Selected: ${randCompSelector}`);
+  return randCompSelector;
+}
+
+function userPlay(btnClicked) {
+  let playerSelection = btnClicked;
+  return playerSelection;
+}
+
+// rock.addEventListener('click', userPlay);
+
+function playRound(playerSelection, compSelection) {
+  if (playerSelection == "rock" && compSelection == "scissors") {
+    playerScore++;
+    return console.log("You Win! Rock beats Scissors");
+  } else if (playerSelection == "scissors" && compSelection == "rock") {
+    compScore++;
+    return console.log("You Lose! Rock beats Scissors");
+  } else if (playerSelection == "rock" && compSelection == "paper") {
+    compScore++;
+    return console.log("You Lose! Paper beats Rock!");
+  } else if (playerSelection == "paper" && compSelection == "rock") {
+    playerScore++;
+    return console.log("You Win! Paper beats Rock!");
+  } else if (playerSelection == "paper" && compSelection == "scissors") {
+    compScore++;
+    return console.log("You Lose! Scissors beats Paper!");
+  } else if (playerSelection == "scissors" && compSelection == "paper") {
+    playerScore++;
+    return console.log("You Win! Scissors beats Paper!");
+  } else {
+    return console.log("Draw! " + playerSelection + " vs " + compSelection + "!");
+  }
+}
+
+// playRound(playerSelection, compSelection);
+
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     const compSelection = computerPlay();
+//     const playerSelection = userPlay();
+//     playRound(playerSelection, compSelection);
+//     console.log("Score is:\n" + "Player: " + playerScore + "\n" + "Computer: " + compScore + "\n");
+//   }
+
+//   if (playerScore > compScore) {
+//     console.log("\nCongratulations! You beat the computer in Rock, Paper, Scissors!");
+//   } else if (playerScore < compScore) {
+//     console.log("\nOh no! Better luck next time!");
+//   } else {
+//     console.log("\nLooks like we got ourselves a draw!");
+//   }
+//   // console.log("computer: " + compSelection);
+//   // console.log("player: " + playerSelection);
+// }
+
+const results = document.querySelector("#results");
+
+// game();
